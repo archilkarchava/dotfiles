2260f4ee446b05c21feadf88835fd10a7debaf80
Archil Karchava <archil1997@gmail.com>
2020-10-22 18:08:02 +0500

    Add alias to edit alacritty configuration

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -31,6 +31,9 @@ alias spotify="${HOME}/scripts/gtk-enforce-theme-variant -qt dark 'LD_PRELOAD=/u
 alias lzd='lazydocker'
 
 alias vicfg="$EDITOR $HOME/.config/nvim/init.vim"
+
+alias vial="$EDITOR $HOME/.config/alacritty/alacritty.yml"
+
 alias vizrc="$EDITOR $ZDOTDIR/.zshrc"
 alias vizp="$EDITOR $ZDOTDIR/.zprofile"
 alias vip="$EDITOR $HOME/.profile"
